ab1fd48dd94c29723b94f22618dd3a88da1b226f
Peter Griffin <peter.griffin@linaro.org>
2020-10-15 11:36:02 +0100

    FindWPEBackend-rdk.cmake: add cmake to pickup wpebackend-rdk headers
    Signed-off-by: Peter Griffin <peter.griffin@linaro.org>

---
+++ Source/cmake/FindWPEBackend-rdk.cmake
@@ -0,0 +1,47 @@
+# - Try to find WPEBackend-rdk.
+# Once done, this will define
+#
+#  WPEBACKEND_RDK_FOUND - system has WPEBackend-rdk.
+#  WPEBACKEND_RDK_INCLUDE_DIRS - the WPEBackend-rdk include directories
+#  WPEBACKEND_RDK_LIBRARIES - link these to use WPEBackend-rdk.
+#
+# Copyright (C) 2020 Linaro
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1.  Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+# 2.  Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND ITS CONTRIBUTORS ``AS
+# IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR ITS
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+find_package(PkgConfig)
+pkg_check_modules(PC_WPEBACKEND_RDK QUIET wpebackend-rdk-1.0)
+
+find_path(WPEBACKEND_RDK_INCLUDE_DIRS
+    NAMES wpe/compositorclient-get-surface.h
+    HINTS ${PC_WPEBACKEND_RDK_INCLUDEDIR} ${PC_WPEBACKEND_RDK_INCLUDE_DIRS}
+)
+
+find_library(WPEBACKEND_RDK_LIBRARIES
+    NAMES WPEBackend-rdk
+    HINTS ${PC_WPEBACKEND_RDK_LIBDIR} ${PC_WPEBACKEND_RDK_LIBRARY_DIRS}
+)
+
+mark_as_advanced(WPEBACKEND_RDK_INCLUDE_DIRS WPEBACKEND_RDK_LIBRARIES)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(WPEBackend-rdk REQUIRED_VARS WPEBACKEND_RDK_INCLUDE_DIRS WPEBACKEND_RDK_LIBRARIES)
